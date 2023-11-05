85b168c72033b5365262b6de2b08594d191f7871
Mir369 <122328479+Mir369@users.noreply.github.com>
2023-11-05 17:15:48 -0500

    adding the main file changes for the tip app

--- lib/main.dart
+++ lib/main.dart
@@ -4,122 +4,270 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+class _MyAppState extends State<MyApp> {
+  double bill = 0.0; //Varibles to help store and do calcultaions with.
+  double tipPercent = 0.0;
+  double tip = 0.0;
+  double tipDecimalPercent = 0.0;
+  double bill2 = 0.0;
+  int amountOfPeople = 0;
+  double splitBill = 0.0;
+  String history = "";
+  //functions for calculating the tip
+  void calculateTip() {
+    tipDecimalPercent = tipPercent / 100.0;
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      tip = bill * tipDecimalPercent;
+      tip = double.parse(tip.toStringAsFixed(2));
+      addHistory(1);
+    });
+  }
+
+//function to help store values into a text in a drawer in the history widget(found in drawer)
+  void addHistory(int x) {
+    setState(() {
+      if (x == 1) {
+        history +=
+            "\nBill: $bill \nTip Percentage: $tipPercent \nTip: $tip\n ____________\n";
+      } else {
+        history +=
+            "\nBill: $bill2 \nNumber of People: $amountOfPeople \nBill for each person: $splitBill\n ____________\n";
+      }
+    });
+  }
+
+//function to help calculate the amount of the bill spilt between a certian amount of people
+  void calculateSplitBill() {
+    setState(() {
+      if (amountOfPeople == 0) {
+        //checks to get rid of /0 error
+        splitBill = bill2;
+      } else {
+        splitBill = bill2 / amountOfPeople;
+        splitBill = double.parse(splitBill.toStringAsFixed(2));
+      }
+      addHistory(2);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return MaterialApp(
+        //base of the app containing the scaffold with the appbar, body, and the drawer.
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: const Color.fromARGB(255, 44, 46, 47),
+          appBar: AppBar(
+            //app bar
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(30),
+              bottomLeft: Radius.circular(30),
+            )),
+            backgroundColor: const Color.fromARGB(255, 19, 17, 121),
+            title: const Text("Tip Calclator"),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                    width: 400,
+                    height: 50,
+                    margin: const EdgeInsets.only(bottom: 10),
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 2, style: BorderStyle.solid),
+                        color: const Color.fromARGB(255, 5, 196, 238),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Center(
+                        child: TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Bill',
+                      ),
+                      onChanged: (value) {
+                        if (double.tryParse(value) != null) {
+                          if (value.length <= 9) {
+                            bill = double.parse(value);
+                          } else {
+                            bill = 0;
+                          }
+                        } else {
+                          bill = 0;
+                        }
+                      },
+                    ))),
+                Container(
+                    width: 400,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 2, style: BorderStyle.solid),
+                        color: const Color.fromARGB(255, 5, 196, 238),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Tip Percentage',
+                      ),
+                      onChanged: (value) {
+                        if (double.tryParse(value) != null) {
+                          tipPercent = double.parse(value);
+                        } else {
+                          tipPercent = 0;
+                        }
+                      },
+                    )),
+                Container(
+                  width: 500,
+                  height: 60,
+                  margin: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 5, style: BorderStyle.solid),
+                      color: const Color.fromARGB(255, 214, 255, 230),
+                      borderRadius: BorderRadius.circular(360)),
+                  child: Text(
+                    "  Tip: $tip",
+                    style: const TextStyle(
+                        fontSize: 40, fontStyle: FontStyle.italic),
+                  ),
+                ),
+                Container(
+                    width: 140,
+                    height: 140,
+                    margin: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: Colors.redAccent[700],
+                        borderRadius: BorderRadius.circular(500),
+                        border: Border.all(
+                            width: 10, color: const Color(0xff030303))),
+                    child: TextButton(
+                      onPressed: calculateTip,
+                      child: const Center(
+                        child: Text(
+                          "Enter",
+                          style: TextStyle(fontSize: 28, color: Colors.white),
+                        ),
+                      ),
+                    )),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          drawer: Drawer(
+            backgroundColor: const Color.fromARGB(255, 159, 180, 182),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  const Text(
+                    "Extra Features",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.tealAccent, fontSize: 30),
+                  ),
+                  Container(
+                      width: 300,
+                      height: 350,
+                      decoration: BoxDecoration(
+                          color: const Color(0xfff4c3db),
+                          borderRadius: BorderRadius.circular(60),
+                          border: Border.all(
+                              width: 5, color: const Color(0xff030303))),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          const Text('Splitting the Bill',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 20, fontStyle: FontStyle.italic)),
+                          TextField(
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                              border: UnderlineInputBorder(),
+                              fillColor: Color.fromRGBO(137, 128, 128, 1),
+                              labelText: 'Bill',
+                            ),
+                            onChanged: (value) {
+                              if (double.tryParse(value) != null) {
+                                bill2 = double.parse(value);
+                              } else {
+                                bill2 = 0;
+                              }
+                            },
+                          ),
+                          TextField(
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                              border: UnderlineInputBorder(),
+                              fillColor: Color.fromRGBO(137, 128, 128, 1),
+                              labelText: 'Amount of people',
+                            ),
+                            onChanged: (value) {
+                              if (int.tryParse(value) != null) {
+                                amountOfPeople = int.parse(value);
+                              } else {
+                                amountOfPeople = 0;
+                              }
+                            },
+                          ),
+                          Text('Split bill between each person: $splitBill'),
+                          Container(
+                              width: 80,
+                              height: 80,
+                              margin: const EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  color: Colors.redAccent[700],
+                                  borderRadius: BorderRadius.circular(360),
+                                  border: Border.all(
+                                      width: 5,
+                                      color: const Color(0xff030303))),
+                              child: TextButton(
+                                onPressed: calculateSplitBill,
+                                child: const Center(
+                                  child: Text(
+                                    "Enter",
+                                    style: TextStyle(
+                                        fontSize: 20, color: Colors.white),
+                                  ),
+                                ),
+                              )),
+                        ],
+                      )),
+                  Container(
+                    width: 300,
+                    height: 200,
+                    decoration: BoxDecoration(
+                        color: const Color(0xff2f8dcb),
+                        borderRadius: BorderRadius.circular(60),
+                        border: Border.all(
+                            width: 5, color: const Color(0xff030303))),
+                    child: ListView(
+                      children: [
+                        const Text(
+                          "History",
+                          style: TextStyle(fontSize: 20),
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          history,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(fontSize: 15),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+          ),
+        ));
   }
 }
