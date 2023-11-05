722e81a02ad2e539deccacef2dfede08c214de64
Mir369 <122328479+Mir369@users.noreply.github.com>
2023-11-05 17:27:39 -0500

    added the final comments to explain the app features

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,11 @@ class _MyAppState extends State<MyApp> {
   double tipPercent = 0.0;
   double tip = 0.0;
   double tipDecimalPercent = 0.0;
-  double bill2 = 0.0;
+  double bill2 = 0.0; //storing the bill for a different feature
   int amountOfPeople = 0;
-  double splitBill = 0.0;
-  String history = "";
+  double splitBill = 0.0; //data for the extra feature where the bill is split between an amount of people
+  String history = ""; //saving the information in the string that can be used later on.
+
   //functions for calculating the tip
   void calculateTip() {
     tipDecimalPercent = tipPercent / 100.0;
@@ -72,15 +73,15 @@ class _MyAppState extends State<MyApp> {
               bottomLeft: Radius.circular(30),
             )),
             backgroundColor: const Color.fromARGB(255, 19, 17, 121),
-            title: const Text("Tip Calclator"),
+            title: const Text("Tip Calclator"), //name of the app
           ),
-          body: Center(
+          body: Center( //actual tip calculator
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Container(
-                    width: 400,
+                    width: 400, //text box for putting in thr bill.
                     height: 50,
                     margin: const EdgeInsets.only(bottom: 10),
                     decoration: BoxDecoration(
@@ -103,10 +104,10 @@ class _MyAppState extends State<MyApp> {
                           }
                         } else {
                           bill = 0;
-                        }
+                        } //checks the input of the bill and stores it to the bill location
                       },
                     ))),
-                Container(
+                Container( //making the test box for the tip percentage
                     width: 400,
                     height: 50,
                     decoration: BoxDecoration(
@@ -119,7 +120,7 @@ class _MyAppState extends State<MyApp> {
                         border: OutlineInputBorder(),
                         labelText: 'Tip Percentage',
                       ),
-                      onChanged: (value) {
+                      onChanged: (value) { //checks the input given for the tip percent
                         if (double.tryParse(value) != null) {
                           tipPercent = double.parse(value);
                         } else {
@@ -127,7 +128,7 @@ class _MyAppState extends State<MyApp> {
                         }
                       },
                     )),
-                Container(
+                Container( //box to display thr tip that the person would pay
                   width: 500,
                   height: 60,
                   margin: const EdgeInsets.all(10),
@@ -136,12 +137,12 @@ class _MyAppState extends State<MyApp> {
                       color: const Color.fromARGB(255, 214, 255, 230),
                       borderRadius: BorderRadius.circular(360)),
                   child: Text(
-                    "  Tip: $tip",
+                    "  Tip: $tip",//the tip is calculated and shown in this widget
                     style: const TextStyle(
                         fontSize: 40, fontStyle: FontStyle.italic),
                   ),
                 ),
-                Container(
+                Container(//enter button where the claculation happens
                     width: 140,
                     height: 140,
                     margin: const EdgeInsets.all(10),
@@ -151,7 +152,7 @@ class _MyAppState extends State<MyApp> {
                         border: Border.all(
                             width: 10, color: const Color(0xff030303))),
                     child: TextButton(
-                      onPressed: calculateTip,
+                      onPressed: calculateTip, //function of the enter button to get the tip
                       child: const Center(
                         child: Text(
                           "Enter",
@@ -162,18 +163,18 @@ class _MyAppState extends State<MyApp> {
               ],
             ),
           ),
-          drawer: Drawer(
+          drawer: Drawer( //drawer with the scaffold to hold other features for the app like splitting thr bill and the history
             backgroundColor: const Color.fromARGB(255, 159, 180, 182),
             child: Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: <Widget>[
-                  const Text(
+                  const Text( //title for the drawer
                     "Extra Features",
                     textAlign: TextAlign.center,
                     style: TextStyle(color: Colors.tealAccent, fontSize: 30),
                   ),
-                  Container(
+                  Container( //box to show the split bill feature
                       width: 300,
                       height: 350,
                       decoration: BoxDecoration(
@@ -184,11 +185,11 @@ class _MyAppState extends State<MyApp> {
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: <Widget>[
-                          const Text('Splitting the Bill',
+                          const Text('Splitting the Bill', //title of the box for the extra feature
                               textAlign: TextAlign.center,
                               style: TextStyle(
                                   fontSize: 20, fontStyle: FontStyle.italic)),
-                          TextField(
+                          TextField( //input for the bill which is stored in the varible bill2
                             keyboardType: TextInputType.number,
                             decoration: const InputDecoration(
                               border: UnderlineInputBorder(),
@@ -200,10 +201,10 @@ class _MyAppState extends State<MyApp> {
                                 bill2 = double.parse(value);
                               } else {
                                 bill2 = 0;
-                              }
+                              }//checks and validates the input
                             },
                           ),
-                          TextField(
+                          TextField( //textbox to put in the amount of people the bill will be split to 
                             keyboardType: TextInputType.number,
                             decoration: const InputDecoration(
                               border: UnderlineInputBorder(),
@@ -216,10 +217,10 @@ class _MyAppState extends State<MyApp> {
                               } else {
                                 amountOfPeople = 0;
                               }
-                            },
+                            }, //checks the value of the people (if zero, it will return the orginal bill for the answer)
                           ),
-                          Text('Split bill between each person: $splitBill'),
-                          Container(
+                          Text('Split bill between each person: $splitBill'), //displaying the split bill
+                          Container( //enter button very simaler to the one that calculates tip
                               width: 80,
                               height: 80,
                               margin: const EdgeInsets.all(10),
@@ -230,7 +231,7 @@ class _MyAppState extends State<MyApp> {
                                       width: 5,
                                       color: const Color(0xff030303))),
                               child: TextButton(
-                                onPressed: calculateSplitBill,
+                                onPressed: calculateSplitBill, //function to calculate split bill
                                 child: const Center(
                                   child: Text(
                                     "Enter",
@@ -241,7 +242,7 @@ class _MyAppState extends State<MyApp> {
                               )),
                         ],
                       )),
-                  Container(
+                  Container( //new box to store the history of claculations
                     width: 300,
                     height: 200,
                     decoration: BoxDecoration(
@@ -249,15 +250,15 @@ class _MyAppState extends State<MyApp> {
                         borderRadius: BorderRadius.circular(60),
                         border: Border.all(
                             width: 5, color: const Color(0xff030303))),
-                    child: ListView(
+                    child: ListView( //sliding view to see the long text string that stores the values
                       children: [
                         const Text(
-                          "History",
+                          "History", //title of the box
                           style: TextStyle(fontSize: 20),
                           textAlign: TextAlign.center,
                         ),
                         Text(
-                          history,
+                          history,//the history is stored in this varibles and is displayed
                           textAlign: TextAlign.center,
                           style: const TextStyle(fontSize: 15),
                         )
